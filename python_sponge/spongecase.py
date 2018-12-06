e9d8f28e23d21021e18c4e2ea86ef5dcd2d8aa94
Nick Davis <nickdavisa@gmail.com>
2018-12-06 00:04:20 -0700

    fixed issue where spaces and non letters would throw off spongecasing

--- python_sponge/spongecase.py
+++ python_sponge/spongecase.py
@@ -1,17 +1,18 @@
 def spongify(user_string):
     char_count = 0
     spongecase = ''
+    acceptable_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     user_string = user_string.upper()
 
     for i in range(len(user_string)):
-        if user_string[i] != ' ':
-            if char_count % 2 == 1:
-                spongecase += user_string[i]
-            elif char_count % 2 == 0:
-                spongecase += user_string[i].lower()
+        if user_string[i] in acceptable_chars and char_count % 2 == 1:
+            spongecase += user_string[i]
+            char_count += 1
+        elif user_string[i] in acceptable_chars and char_count % 2 == 0:
+            spongecase += user_string[i].lower()
+            char_count += 1
         else:
-            spongecase += ' '
-        char_count += 1
+            spongecase += user_string[i]
 
     return spongecase
 
